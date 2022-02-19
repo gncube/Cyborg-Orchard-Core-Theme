7ab9d3f99836b055e0ca15e4de2cf885113c4b82
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2022-02-19 19:44:26 +0000

    second commit with theme

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Cyborg Theme",
+    Author = "Ncube Digital",
+    Website = "https://ncube.digital",
+    Version = "1.3.3",
+    Description = "Cyborg, Jet black and electric blue theme based on Bootstrap. Icons from Font Awesome. Web fonts from Google. Made by Thomas Park. Code released under the MIT License. This theme was created using v1.0.0 of Etch.OrchardCore.ThemeBoilerplate template."
+)]
